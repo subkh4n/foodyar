6a2d0703e1fcd375bde49e0d8e1293067de04e73
subkh4n <subkh4n@gmail.com>
2021-09-17 14:42:28 +0700

    cob

--- asset/style/style.js
+++ asset/style/style.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
   let featuredFoods = document.querySelectorAll('.bintang');
 
   let starIcon = document.createElement('i');
-  starIcon.classList.add('far', 'fa-star');
+  starIcon.classList.add('fas', 'fa-star');
 
   featuredFoods.forEach((el) => {
     el.append(starIcon, starIcon, starIcon, starIcon, starIcon);
